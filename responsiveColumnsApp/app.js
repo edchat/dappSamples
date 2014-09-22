2e892416afd8a1db10b91ce6d1e2f381b0a7a060
Ed Chatelain <ed.chatelain@gmail.com>
2014-09-22 16:17:06 -0400

    Better work on the ResponsiveColumnApp sample, the layout seems better now.

--- responsiveColumnsApp/app.js
+++ responsiveColumnsApp/app.js
@@ -5,8 +5,12 @@ require(["dapp/Application", "requirejs-text/text!./config.json"],
 		var jsonData = config;
 		jsonData = jsonData.replace(/\/\*.*?\*\//g, "");
 		jsonData = jsonData.replace(/\/\/.*/g, "");
-		var appDeferred = new Application(JSON.parse(jsonData));
-		appDeferred.then(function (app) {
+		new Application(JSON.parse(jsonData)).then(function (app) {
 			console.log("deferred resolved for new App [" + app.id + "] it should be started and default views shown");
+			//document.getElementById("outerDiv").style.display = "block";
+			// by this point things in the sidePane and ViewStack are already visible, but not the header and footer
+			document.getElementById("outerDiv").style.visibility = "visible";
 		});
+		document.getElementById("outerDiv").style.display = "block"; // if the outerDiv is display none things wont show
+	//	document.getElementById("outerDiv").style.visibility = "visible"; // visibility can wait until app is ready
 	});
